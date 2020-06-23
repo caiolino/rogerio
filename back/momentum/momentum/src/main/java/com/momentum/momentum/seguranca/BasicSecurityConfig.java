dba7c3ca802a02be5c83d5ccb92d1fb29dc8d0bd
Denise Pereira <denisefeds@hotmail.com>
2020-06-22 21:52:33 -0300

    Pagina de perfil

--- back/momentum/momentum/src/main/java/com/momentum/momentum/seguranca/BasicSecurityConfig.java
+++ back/momentum/momentum/src/main/java/com/momentum/momentum/seguranca/BasicSecurityConfig.java
@@ -33,6 +33,7 @@ public class BasicSecurityConfig  extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, "/postagem").permitAll()
+		.antMatchers(HttpMethod.POST, "/postagem").permitAll()
 		.antMatchers(HttpMethod.GET, "/postagem/**").permitAll()
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
